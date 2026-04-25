8abf8d1315aa12b996c333749bc74c323101482e
KrzysztofDubel <kdubel@griddynamics.com>
2026-04-25 15:11:31 +0200

    fix: nvim lua plugins

--- nvim/lua/plugins/ros2_kitty.lua
+++ nvim/lua/plugins/ros2_kitty.lua
@@ -3,7 +3,6 @@ return {
   -- Seamless navigation between Kitty and Neovim splits
   {
     "knubie/vim-kitty-navigator",
-    build = "cp ./*.py ~/.config/kitty/",
     keys = {
       { "<C-h>", "<Cmd>KittyNavigateLeft<CR>", mode = {"n", "v", "i"}, desc = "Navigate Left" },
       { "<C-j>", "<Cmd>KittyNavigateDown<CR>", mode = {"n", "v", "i"}, desc = "Navigate Down" },
@@ -12,18 +11,25 @@ return {
     }
   },
   
-  -- ROS 2 Telescope Integration (Find ROS packages, topics, and nodes easily)
   {
     "ErickKramer/nvim-ros2",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-      require("nvim-ros2").setup({
-        telescope = true,
-      })
-    end,
+    opts = {
+      -- picker = "telescope", -- Default
+      autocmds = true,
+      treesitter = true,
+    },
+    keys = {
+      { "<leader>li", function() require("nvim-ros2").pickers.interfaces() end, desc = "[ROS 2]: List interfaces" },
+      { "<leader>ln", function() require("nvim-ros2").pickers.nodes() end, desc = "[ROS 2]: List nodes" },
+      { "<leader>la", function() require("nvim-ros2").pickers.actions() end, desc = "[ROS 2]: List actions" },
+      { "<leader>lt", function() require("nvim-ros2").pickers.topics_info() end, desc = "[ROS 2]: List topics with info" },
+      { "<leader>le", function() require("nvim-ros2").pickers.topics_echo() end, desc = "[ROS 2]: List topics with echo" },
+      { "<leader>ls", function() require("nvim-ros2").pickers.services() end, desc = "[ROS 2]: List services" },
+    },
   },
 }
-
