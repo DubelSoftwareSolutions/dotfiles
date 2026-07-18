adb31c70e19217f3e54f205afd788a727b8030ac
KrzysztofDubel <kdubel@griddynamics.com>
2026-07-18 23:55:53 +0200

    feature: leader o newline keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,7 @@ vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true })
 
 -- Forward word deletion
 vim.keymap.set("i", "<C-Del>", "<C-o>dw", { noremap = true })
+
+-- Newline
+vim.keymap.set("n", "<leader>o", "<cmd>put = ''<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>O", "<cmd>put! = ''<CR>", { noremap = true })
